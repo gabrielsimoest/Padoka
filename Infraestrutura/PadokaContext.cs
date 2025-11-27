497085cc6e818aa16ac31c353590da56b845ac9e
Gabriel Simões <gabrielsimoest@gmail.com>
2025-11-27 14:52:46 -0300

    feat: adicionando banco de dados postgresql

---
+++ Infraestrutura/PadokaContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Padoka.Infraestrutura
+{
+    public class PadokaContext(DbContextOptions<PadokaContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime))
+                    {
+                        property.SetValueConverter(
+                            new ValueConverter<DateTime, DateTime>(
+                                v => DateTime.SpecifyKind(v, DateTimeKind.Utc),
+                                v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+                            )
+                        );
+                    }
+                    else if (property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetValueConverter(
+                            new ValueConverter<DateTime?, DateTime?>(
+                                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null,
+                                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : null
+                            )
+                        );
+                    }
+                }
+            }
+        }
+
+        //public DbSet<model> Model { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
+using MedScale.Sys.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Padoka.Infraestrutura;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddFeatures();
+
+builder.Services.AddDbContext<PadokaContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MedScale")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
